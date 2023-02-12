2d9fbcdfee20e2c8957fce625600c5b2c65d58ab
Valer4ikS <maraclee@yandex.ru>
2023-02-13 02:28:41 +0300

    add max1

---
+++ example005_masha/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите ваше имя: ");
+string username = Console.ReadLine();
+
+if(username == "Баша");
+{
+    Console.WriteLine("Ура, да это же Маша");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine("username");
+}
