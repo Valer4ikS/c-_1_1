77e326035d6f555a06d0935e3a7b6bd20a9ab310
Valer4ikS <maraclee@yandex.ru>
2023-02-17 16:17:38 +0300

    add 8-11

---
+++ example010_MethodArray/Program.cs
@@ -0,0 +1,16 @@
+﻿int[] array = { 21, 42, 21, 15, 21, 55, 22, 54 };
+
+
+int n = array.Length;
+int find = 21;
+int index = 0;
+while (index < n)
+{
+    if(array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    // index = index + 1;
+    index++;
+}
---
+++ example011_ArrayLibrary/Program.cs
@@ -0,0 +1,73 @@
+﻿// void FillArray(int[] collection)
+// {
+//     int length = collection.Length;
+//     int index = 0;
+//     while (index < length)
+//     {
+//         collection[index] = new Random().Next(1, 10);
+//         index ++; // index = index + 1
+//     }
+// }
+
+void PrintArray(int[] col)
+{
+    int count = col.Length;
+    int position = 0;
+    while (position < count)
+    {
+        Console.WriteLine(col[position]);
+        position++;
+    }
+}
+
+int IndexOf(int[] collection, int find)
+{
+    int count = collection.Length;
+    int index = 0;
+    int position = -1;
+    while (index < count)
+    {
+        if (collection[index] == find)
+        {
+            position = index;
+            break;
+        }
+        index++;
+    }
+    return position;
+
+}
+
+int[] array = new int[10];
+
+FillArray(array);
+array[4] = 3;
+array[7] = 2;
+PrintArray(array);
+Console.WriteLine();
+
+void FillArray(int[] collection)
+{
+    int length = collection.Length;
+    int index = 0;
+    while (index < length)
+    {
+        collection[index] = new Random().Next(1, 10);
+        index++;
+
+
+
+
+// int pos = IndexOf(array, 414);
+// Console.WriteLine(pos);
+
+// void FillArray(int[] collection)
+// {
+//     int length = collection.Length;
+//     int index = 0;
+//     while (index < length)
+//     {
+//         collection[index] = new Random().Next(1, 10);
+//         index ++;
+//     }
+// }
